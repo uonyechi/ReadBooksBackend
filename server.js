b6bfb9c7e2d232461bf4ff188f3cb9fb427e62a7
uonyechi <61763942+uonyechi@users.noreply.github.com>
2020-04-01 12:59:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Book'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('Users');
+mobileApp.tables.add('NoteBook');
+mobileApp.tables.add('Note');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
